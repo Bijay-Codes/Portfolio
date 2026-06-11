d89edcb53d1175dc147026a2c4c40920ba7fd08e
Bijay-Codes <bijayprojects@gmail.com>
2026-06-11 17:06:28 +0530

    fix overflowing sketches in phone

--- src/project-codes/components/sketches.jsx
+++ src/project-codes/components/sketches.jsx
@@ -3,7 +3,7 @@ export function RenderSketches() {
     return (
         <section className="p-2 mb-15">
             <h1 className="text-3xl primary-font text-(--pencil-black) mb-6">My Sketches</h1>
-            <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-6 px-4">
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-6 sm:px-4">
                 {portfolioData.sketches.map((sketch, i) => {
                     return (
                         <img key={i}
